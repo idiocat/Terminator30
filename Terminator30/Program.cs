00da3ad4d75f6dfd08f2ec9591b92139971e740e
idiocy <idiocy@DESKTOP-PP32I9J>
2024-11-13 21:29:37 +0300

    Add project files.

---
+++ Terminator30/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace Terminator;
+class Program
+{
+    public static void Main()
+    {
+        string dirName = @"E:\\test";
+        Terminate(dirName);
+
+        Console.ReadKey();
+    }
+
+    static void Terminate(string dirName)
+    {
+        if (Directory.Exists(dirName))
+        {
+            string[] files = Directory.GetFiles(dirName);
+            foreach (string file in files) {
+                try { if (DateTime.Now - File.GetLastWriteTime(file) > TimeSpan.FromMinutes(30)) { File.Delete(file); } }
+                catch (Exception e) { Console.WriteLine($"Error: {e}"); }
+            }
+            string[] dirs = Directory.GetDirectories(dirName);
+            foreach (string dir in dirs) {
+                if (DateTime.Now - Directory.GetLastWriteTime(dir) > TimeSpan.FromMinutes(30))
+                {
+                    Terminate(dir);
+                    try { Directory.Delete(dir); } catch { }
+                }
+            }
+        }
+
+    }
+}
